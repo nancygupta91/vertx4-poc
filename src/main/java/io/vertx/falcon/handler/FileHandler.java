52b338ecd7d997395a2b899083b1af19ff1fd45a
Gupta, Nancy <Nancy.gupta@unisys.com>
2021-01-06 18:46:53 +0530

    File changes

--- src/main/java/io/vertx/falcon/handler/FileHandler.java
+++ src/main/java/io/vertx/falcon/handler/FileHandler.java
@@ -1,6 +1,7 @@
 package io.vertx.falcon.handler;
 
 import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.FileUpload;
 import io.vertx.ext.web.RoutingContext;
@@ -11,9 +12,10 @@ public class FileHandler implements Handler<RoutingContext> {
     public void handle(RoutingContext routingContext) {
         if (routingContext.fileUploads() != null && !routingContext.fileUploads().isEmpty()) {
 
-            System.out.println(routingContext.vertx().getOrCreateContext());
-            JsonObject json1 = routingContext.vertx().getOrCreateContext().get("TestKey");
+            System.out.println(Vertx.currentContext());
+            JsonObject json1 = Vertx.currentContext().get("TestKey");
             System.out.println(json1);
+            System.out.println(routingContext.vertx().getOrCreateContext());
             JsonObject json2 = routingContext.vertx().getOrCreateContext().get("TestKey");
             System.out.println(json2);
             FileUpload fileUpload = routingContext.fileUploads().iterator().next();
